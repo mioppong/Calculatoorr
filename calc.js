ac5126527e1f370193a57d9af3bd23c77e3ae508
mioppong <michaeloppong123@gmail.com>
2019-09-29 09:26:04 -0400

    mostly done with css, starting with javascript

--- calc.js
+++ calc.js
@@ -1,23 +1,11 @@
-function myFunction() {
-  //document.getElementById("demo").style.backgroundColor = "red";
-  document.getElementById("calcDisplay").innerHTML++;
-}
+var buttons = document.getElementsByTagName("div.button");
+document.getElementById("1").onclick = function(e){
+	alert("tes");
+};
 
-function myFunction2() {
- 
-  var a=parseInt(document.getElementById("foo").value);
-
-  switch (true){
-	case (a>89): 
-		answer="A+";
-		break;
-	case (a>79): 
-		answer="A";
-		break;
-	default:
-		answer="F";
-  }
-  
-  document.getElementById("output").innerHTML=answer; 
-
+var buttonsCount = buttons.length;
+for (var i = 0; i <= buttonsCount; i += 1) {
+    buttons[i].onclick = function(e) {
+        alert(this.id);
+    };
 }
